8d6445eef0face9f82af0cf50a33a89d1e97d444
Volodymyr Kulmatytskyy <kulma@fellow.33mail.com>
2017-02-20 20:20:58 +0200

    add JDBC config and test controller method

--- src/main/java/kma/upp/neruhomist/controller/TestController.java
+++ src/main/java/kma/upp/neruhomist/controller/TestController.java
@@ -1,13 +1,19 @@
 package kma.upp.neruhomist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/api/helloworld")
     public ResponseEntity<String> testHelloworldApi() {
@@ -17,6 +23,12 @@ public class TestController {
     @RequestMapping("/jsp/helloworld")
     public String testHelloworldJsp() {
         return "helloworld";
+    }
+
+    @RequestMapping("/db")
+    public ResponseEntity<String> testDb() {
+        String result = jdbcTemplate.queryForObject("select 'OK'", String.class);
+        return result == null ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("DB test request failed") : ResponseEntity.ok(result);
     }
 
 }
