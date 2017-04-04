f02cea45dbbfed4cc4c39f240c389f28048620b2
Volodymyr Kuryliak <vvkuryliak@gmail.com>
2017-04-04 04:55:59 +0300

    implement client table displaying

--- src/main/java/kma/upp/neruhomist/ui/ListClientsFrame.java
+++ src/main/java/kma/upp/neruhomist/ui/ListClientsFrame.java
@@ -1,6 +1,9 @@
 package kma.upp.neruhomist.ui;
 
+import kma.upp.neruhomist.repository.ClientRepository;
+import kma.upp.neruhomist.ui.util.ClientTableModel;
 import kma.upp.neruhomist.ui.util.DelayedInitJFrame;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
@@ -12,69 +15,58 @@ public class ListClientsFrame extends DelayedInitJFrame {
 
     private javax.swing.JButton backButton;
     private javax.swing.JButton moreAboutClientButton;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private JButton serachButton;
+    private javax.swing.JLabel phoneLabel;
+    private javax.swing.JPanel clientsListPanel;
+    private javax.swing.JPanel backButtonPanel;
+    private javax.swing.JPanel searchPanel;
+    private javax.swing.JScrollPane tableScrollPane;
+    private javax.swing.JTable clientsTable;
+    private javax.swing.JTextField phoneToSearchField;
+    private JButton searchButton;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    private ClientTableModel tableModel;
 
     @Override
 	protected void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jPanel1.setBounds(0, 70, 325, 300);
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        clientsListPanel = new javax.swing.JPanel();
+        clientsListPanel.setBounds(0, 70, 325, 300);
+        tableScrollPane = new javax.swing.JScrollPane();
+        clientsTable = new javax.swing.JTable();
         moreAboutClientButton = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel2.setBounds(0, 380, 325, 40);
+        backButtonPanel = new javax.swing.JPanel();
+        backButtonPanel.setBounds(0, 380, 325, 40);
         backButton = new javax.swing.JButton();
-        jPanel3 = new javax.swing.JPanel();
-        jPanel3.setBounds(0, 0, 325, 70);
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        searchPanel = new javax.swing.JPanel();
+        searchPanel.setBounds(0, 0, 325, 70);
+        phoneLabel = new javax.swing.JLabel();
+        phoneToSearchField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Список клієнтів");
         getContentPane().setLayout(null);
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Список клієнтів"));
-        jPanel1.setName("panelOwners"); // NOI18N
+        clientsListPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Список клієнтів"));
+        clientsListPanel.setName("panelOwners"); // NOI18N
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {"Джек Деніелс", "фізична", "99878766"},
-                {"Курит Воннегут", "юридична", "7675433"},
-                {"Дарья Донцова", "фізична", "9379992"},
-                {null, null, null}
-            },
-            new String [] {
-                "Клієнт", "Тип особи", "Телефон"
-            }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-        });
-        jTable1.setName("tableOwners"); // NOI18N
-        jScrollPane1.setViewportView(jTable1);
+        tableModel = new ClientTableModel(clientRepository.findAll());
+        clientsTable.setModel(tableModel);
+        clientsTable.setName("tableOwners"); // NOI18N
+        clientsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tableScrollPane.setViewportView(clientsTable);
 
         moreAboutClientButton.setText("Детальніше про клієнта...");
         moreAboutClientButton.setName("buttonOwnerDetails"); // NOI18N
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(clientsListPanel);
         jPanel1Layout.setHorizontalGroup(
         	jPanel1Layout.createParallelGroup(Alignment.LEADING)
         		.addGroup(jPanel1Layout.createSequentialGroup()
         			.addGroup(jPanel1Layout.createParallelGroup(Alignment.LEADING)
-        				.addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 311, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(tableScrollPane, GroupLayout.PREFERRED_SIZE, 311, GroupLayout.PREFERRED_SIZE)
         				.addGroup(jPanel1Layout.createSequentialGroup()
         					.addGap(53)
         					.addComponent(moreAboutClientButton, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)))
@@ -84,21 +76,21 @@ public class ListClientsFrame extends DelayedInitJFrame {
         	jPanel1Layout.createParallelGroup(Alignment.LEADING)
         		.addGroup(jPanel1Layout.createSequentialGroup()
         			.addContainerGap()
-        			.addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
+        			.addComponent(tableScrollPane, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)
         			.addGap(18)
         			.addComponent(moreAboutClientButton)
         			.addContainerGap(18, Short.MAX_VALUE))
         );
-        jPanel1.setLayout(jPanel1Layout);
+        clientsListPanel.setLayout(jPanel1Layout);
 
-        getContentPane().add(jPanel1);
+        getContentPane().add(clientsListPanel);
 
-        jPanel2.setName("panelNazad"); // NOI18N
+        backButtonPanel.setName("panelNazad"); // NOI18N
 
         backButton.setText("Назад до головного меню");
         backButton.setName("buttonNazad"); // NOI18N
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(backButtonPanel);
         jPanel2Layout.setHorizontalGroup(
         	jPanel2Layout.createParallelGroup(Alignment.LEADING)
         		.addGroup(jPanel2Layout.createSequentialGroup()
@@ -113,30 +105,30 @@ public class ListClientsFrame extends DelayedInitJFrame {
         			.addComponent(backButton)
         			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel2.setLayout(jPanel2Layout);
+        backButtonPanel.setLayout(jPanel2Layout);
 
-        getContentPane().add(jPanel2);
+        getContentPane().add(backButtonPanel);
 
-        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Пошук"));
-        jPanel3.setName("panelPoshuk"); // NOI18N
+        searchPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Пошук"));
+        searchPanel.setName("panelPoshuk"); // NOI18N
 
-        jLabel1.setText("Телефон");
-        jLabel1.setName("labalTelefon"); // NOI18N
+        phoneLabel.setText("Телефон");
+        phoneLabel.setName("labalTelefon"); // NOI18N
 
-        jTextField1.setName("textfieldTelefon"); // NOI18N
+        phoneToSearchField.setName("textfieldTelefon"); // NOI18N
         
-        serachButton = new JButton("Пошук");
+        searchButton = new JButton("Пошук");
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(searchPanel);
         jPanel3Layout.setHorizontalGroup(
         	jPanel3Layout.createParallelGroup(Alignment.LEADING)
         		.addGroup(jPanel3Layout.createSequentialGroup()
         			.addContainerGap()
-        			.addComponent(jLabel1)
+        			.addComponent(phoneLabel)
         			.addPreferredGap(ComponentPlacement.UNRELATED)
-        			.addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+        			.addComponent(phoneToSearchField, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
         			.addPreferredGap(ComponentPlacement.RELATED)
-        			.addComponent(serachButton, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+        			.addComponent(searchButton, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
         			.addContainerGap(32, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
@@ -144,20 +136,24 @@ public class ListClientsFrame extends DelayedInitJFrame {
         		.addGroup(jPanel3Layout.createSequentialGroup()
         			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         			.addGroup(jPanel3Layout.createParallelGroup(Alignment.BASELINE)
-        				.addComponent(jLabel1)
-        				.addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(serachButton))
+        				.addComponent(phoneLabel)
+        				.addComponent(phoneToSearchField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(searchButton))
         			.addContainerGap(16, Short.MAX_VALUE))
         );
-        jPanel3.setLayout(jPanel3Layout);
+        searchPanel.setLayout(jPanel3Layout);
 
-        getContentPane().add(jPanel3);
+        getContentPane().add(searchPanel);
 
         attachActionsToButtons();
     }
 
     private void attachActionsToButtons() {
         backButton.addActionListener(backToMenu);
+        searchButton.addActionListener(actionEvent -> {
+            tableModel.setClients(clientRepository.findByPhoneStartingWith(phoneToSearchField.getText()));
+            tableModel.fireTableDataChanged();
+        });
     }
 
 
---
+++ src/main/java/kma/upp/neruhomist/ui/util/ClientTableModel.java
@@ -0,0 +1,47 @@
+package kma.upp.neruhomist.ui.util;
+
+import kma.upp.neruhomist.model.Client;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class ClientTableModel extends AbstractTableModel {
+
+    private static final String[] HEADERS = new String[]{"Клієнт", "Тип особи", "Телефон"};
+
+    private List<Client> clients;
+
+    public ClientTableModel(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    @Override
+    public int getRowCount() {
+        return clients.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        switch (column) {
+            case 0: return clients.get(row).getSurname();
+            case 1: return clients.get(row).getType();
+            case 2: return clients.get(row).getPhone();
+            default:return null;
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+       return HEADERS[column];
+    }
+
+}
